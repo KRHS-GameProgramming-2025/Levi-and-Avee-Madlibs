7393e7be56ac1e0292293fdcebd9fb545a4c2cca
Avee <owen.salmon@kearsarge.org>
2024-11-08 09:03:03 -0500

    updates

--- madlibstory4.py
+++ madlibstory4.py
@@ -17,3 +17,7 @@ def story4(debug=False):
     out+=" in town " 
     return out
 
+#wip  Me and my friend = name FN1 and my other friend FN2, went to the ballpark and got some hotdogs and FD1 along with SE1 to season it!
+#it was awfully delicious! im glad i know what kind of food it was! itd be a shame if i didnt.. oh well! me and FN1 + FN2 had a lovely time!
+#notes from Dev1, FN1 = friendname1 and FN2 is friend name two, and FD1 is food1 and SE1 is seasoning1, and ive included a hint for the easteregg, and you can use the comments
+#just put yourself as Dev2
